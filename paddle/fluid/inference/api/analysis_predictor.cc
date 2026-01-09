c7b1ec9f30d4d1a54a5572fb5fa196d9cd51b5bf
sdcb <sdflysha@qq.com>
2026-01-09 18:39:54 +0800

    Try build v3.2

--- paddle/fluid/inference/api/analysis_predictor.cc
+++ paddle/fluid/inference/api/analysis_predictor.cc
@@ -464,7 +464,9 @@ bool AnalysisPredictor::Init(
   paddle::platform::SetNumThreads(config_.cpu_math_library_num_threads());
 
   std::string model_path = config_.prog_file();
-  if (!model_path.empty()) {
+  if (config_.model_from_memory()) {
+    load_pir_model_ = config_.new_executor_enabled();
+  } else if (!model_path.empty()) {
     load_pir_model_ =
         model_path.substr(model_path.find_last_of(".") + 1) == "json";
   } else if (!config_.model_dir().empty()) {
@@ -1379,7 +1381,8 @@ bool AnalysisPredictor::SaveOrLoadPirParameters(bool for_save) {
       }
 
     } else {
-      if (std::filesystem::exists(config_.params_file())) {
+      if (config_.model_from_memory() ||
+          std::filesystem::exists(config_.params_file())) {
         pir::LoadCombineFunction(config_.params_file(),
                                  filter_param_names,
                                  &tensor_out,
